5c97f299ef4c36195ec0986f9c8d03308c87000b
Cassio Batista <cassio.batista.13@gmail.com>
2015-10-04 22:44:53 -0300

    slide editeD

--- curso_arduino/codes/desafio2.ino
+++ curso_arduino/codes/desafio2.ino
@@ -2,17 +2,18 @@ void loop() {
 	int i = 0;
 	if(Serial.available() > 0) {
 		while(Serial.available() && i<30) 
-			str[i++] = (char) Serial.read();
-		if(str[0]=='A' || str[0]=='a')
+			str[i++] = (char) Serial.read(); //compose string
+		if(str[0]=='A' || str[0]=='a') //amarelo
 			digitalWrite(LED_Y, HIGH);
-		else if (str[0]=='V' && strlen(str)==1)
+		//else if (strcmp(str, "V"==0)
+		else if (str[0]=='V' && strlen(str)==1) //verde
 			digitalWrite(LED_G, HIGH);
 		else if (strlen(str)==3) {
 			for(i=0; i<strlen(str); i++)
-				if(str[i]<91)
-					str[i]=str[i]+('a'-'A');
+				if(str[i]<91) //check ascii code out
+					str[i]=str[i]+('a'-'A'); //tolower()
 			if(strcmp(str, "red")==0)
-				digitalWrite(LED_G, LOW);
+				digitalWrite(LED_G, HIGH);
 		}
 		delay(100);
 	}//close if
